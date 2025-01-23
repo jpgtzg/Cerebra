45c85539661c0c2bdfee0efa6ad55a7b592a323b
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-01-23 16:58:45 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 DESCRIPTION = 'Cerebra is an AI Orchestrator for LLM models.'
 
 # Setting up
