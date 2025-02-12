362f3d9956215221683daa9d07b5542fe40ceb89
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-02-11 18:11:50 -0600

    Update Orchestrator initialization with default weighting parameters

--- cerebraai/models/orchestrator.py
+++ cerebraai/models/orchestrator.py
@@ -8,16 +8,27 @@ from cerebraai.models.llm import LLM, LLMResponse
 class Orchestrator:
 
     llms: list[LLM]
+    analysis_weights: dict
+    sentiment_weights: dict
+    emotion_weights: dict
+
     """
     A class that represents an AI Orchestrator. It will balance prompting to different LLMs based on the user's prompt.
     """
 
-    def __init__(self, llms: list[LLM], text_model: SentenceTransformer):
+    def __init__(self, llms: list[LLM], text_model: SentenceTransformer, analysis_weights: dict = {"semantic": 0.3, "topic": 0.5, "sentiment": 0.3}, sentiment_weights: dict = {"positive": 0.5, "negative": 0.5}, emotion_weights: dict = {"happy": 0.5, "sad": 0.5}):
         self.llms = llms
         self.text_model = text_model
+        self.analysis_weights = analysis_weights
+        self.sentiment_weights = sentiment_weights
+        self.emotion_weights = emotion_weights
 
-    def execute(self, prompt: str, weights: dict = {"semantic": 0.3, "topic": 0.5, "sentiment": 0.3}, sentiment_weights: dict = {"positive": 0.5, "negative": 0.5}, emotion_weights: dict = {"happy": 0.5, "sad": 0.5}) -> LLMResponse:
-        llm = get_best_model(self.text_model, prompt, self.llms, weights, sentiment_weights, emotion_weights)["llm"]
+    def execute(self, prompt: str, analysis_weights: dict = None, sentiment_weights: dict = None, emotion_weights: dict = None) -> LLMResponse:
+        analysis_weights = analysis_weights if analysis_weights is not None else self.analysis_weights
+        sentiment_weights = sentiment_weights if sentiment_weights is not None else self.sentiment_weights
+        emotion_weights = emotion_weights if emotion_weights is not None else self.emotion_weights
+        
+        llm = get_best_model(self.text_model, prompt, self.llms, analysis_weights, sentiment_weights, emotion_weights)["llm"]
         return llm.execute(prompt)
 
     def add_llm(self, llm: LLM):
